63ab88c0b9b6b4d3aea7ea0a6f244b3f6f33c525
Deepak Sampath <76862788+deepaksampath1996@users.noreply.github.com>
2021-01-02 15:00:38 +0530

    Update prime_or_not.py

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,11 +1,6 @@
 n = 3
-def prime(n):
-
-
-    if n ==1:
-        print ("not a prime")
-for d in range(2, n):
-    if n % d==0:
+for num in range(2, n):
+    if n % num==0:
         print ("not a prime")
         break
     else:
