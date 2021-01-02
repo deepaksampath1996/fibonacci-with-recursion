84d5a4c9e7c09d9282819da6fed8da01f62b28a1
Deepak Sampath <76862788+deepaksampath1996@users.noreply.github.com>
2021-01-02 13:06:33 +0530

    Add files via upload

---
+++ fibonacci with recursion.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.http import  JsonResponse
+
+
+def fibonacci(n):
+    if n==1:
+       return n
+    elif n==2:
+      return n
+    elif n>2:
+        return fibonacci(n-1) + fibonacci(n-2)
+for n in range(1,11):
+    print(fibonacci(n))
